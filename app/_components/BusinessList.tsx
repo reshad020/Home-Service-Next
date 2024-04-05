3042a44b66eeee2048678d110f36f3fe3f7f34e1
reshad020 <aehtesham2002@gmail.com>
2024-04-05 06:29:00 +0600

    skeleton loading effect added

--- app/_components/BusinessList.tsx
+++ app/_components/BusinessList.tsx
@@ -35,32 +35,49 @@ function BusinessList(props: BusinessListProps) {
         {props.title}
       </h2>
       <div className={`md:grid ${gridColumnClass} gap-4 mx-4`}>
-        {props.list.map((item) => (
-          <Link
-            href={"/details/" + item.id}
-            key={item.email}
-            className="shadow-lg hover:scale-105 transition-all ease-in-out"
-          >
-            <div className="p-4">
-              <Image
-                src={item?.images[0]?.url}
-                alt={item.name}
-                width={290}
-                height={160}
-                className="rounded-lg w-[300px] h-[220px]"
-              />
-              <h2 className="bg-secondary p-1 w-32 rounded-full text-center my-2">
-                {item.category.name}
-              </h2>
-              <h2 className="text-lg font-semibold my-2">{item.name}</h2>
-              <h2 className="text-primary font-semibold my-2 text-lg">
-                {item.contactPerson}
-              </h2>
-              <p className="text-gray-500 my-2">{item.address}</p>
-              <Button className="bg-primary my-2">Book now</Button>
-            </div>
-          </Link>
-        ))}
+        {
+          props.list.length === 0
+            ? [1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
+                <div
+                  key={index}
+                  className="w-full h-[300px]
+              bg-slate-200 rounded-lg animate-pulse"
+                ></div>
+              ))
+            : props.list.map((item) => (
+                <Link
+                  href={"/details/" + item.id}
+                  key={item.email}
+                  className="shadow-lg hover:scale-105 transition-all ease-in-out"
+                >
+                  <div className="p-4">
+                    <Image
+                      src={item?.images[0]?.url}
+                      alt={item.name}
+                      width={290}
+                      height={160}
+                      className="rounded-lg w-[300px] h-[220px]"
+                    />
+                    <h2 className="bg-secondary p-1 w-32 rounded-full text-center my-2">
+                      {item.category.name}
+                    </h2>
+                    <h2 className="text-lg font-semibold my-2">{item.name}</h2>
+                    <h2 className="text-primary font-semibold my-2 text-lg">
+                      {item.contactPerson}
+                    </h2>
+                    <p className="text-gray-500 my-2">{item.address}</p>
+                    <Button className="bg-primary my-2">Book now</Button>
+                  </div>
+                </Link>
+              ))
+          //   : [1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
+          //       <div
+          //         key={index}
+          //         className="w-full h-[300px]
+          // bg-slate-200 rounded-lg animate-pulse"
+          //       ></div>
+          //     ))}
+        }
       </div>
     </div>
   );
--- app/_components/Categories.tsx
+++ app/_components/Categories.tsx
@@ -14,16 +14,30 @@ interface CategoryProps {
 function Categories(props: CategoryProps) {
   return (
     <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 lg:mx-52 md:mx-20 mx-6 my-10">
-      {props.list.map((item) => (
-        <Link
-          href={"/search/" + item.name}
-          key={item.id}
-          className="flex flex-col items-center justify-center bg-indigo-100 p-6 cursor-pointer  rounded-lg hover:scale-110 transition-all ease-in-out"
-        >
-          <Image src={item.icon.url} alt={item.name} width={40} height={40} />
-          <h2 className="text-primary">{item.name}</h2>
-        </Link>
-      ))}
+      {props.list.length === 0
+        ? [1, 2, 3, 4, 5, 6].map((item, index) => (
+            <div
+              key={index}
+              className="h-[120px]
+          w-full bg-slate-200 animate-pulse
+          rounded-lg"
+            ></div>
+          ))
+        : props.list.map((item) => (
+            <Link
+              href={"/search/" + item.name}
+              key={item.id}
+              className="flex flex-col items-center justify-center bg-indigo-100 p-6 cursor-pointer  rounded-lg hover:scale-110 transition-all ease-in-out"
+            >
+              <Image
+                src={item.icon.url}
+                alt={item.name}
+                width={40}
+                height={40}
+              />
+              <h2 className="text-primary">{item.name}</h2>
+            </Link>
+          ))}
     </div>
   );
 }
